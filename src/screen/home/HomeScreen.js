dd73dfc7e8ba79c154fd0b400ef57079d9b2cf9f
lucianoarmoa98 <mmannuarmoa@gmail.com>
2023-08-01 22:46:02 -0400

    se envia al front un mensaje desde la app

--- src/screen/home/HomeScreen.js
+++ src/screen/home/HomeScreen.js
@@ -1,11 +1,20 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { Component, useEffect, useRef, useState } from 'react';
 import { View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 
 const HomeScreen = () => {
+  const refWebView = useRef(null);
 
     let url = 'http://192.168.0.7:3000'
+
+
+    //recibir mensaje desde el navegador usando el evento onMessage
+    const onMessage = (event) => {
+        console.log('event', event.nativeEvent.data);
+        //enviar mensaje al navegador
+        refWebView.current.postMessage('Luciano');
+    }
 
 
 
@@ -13,8 +22,10 @@ const HomeScreen = () => {
   return (
     <View style={{ flex: 1 }}>
         <WebView
+            ref={refWebView}
             source={{ uri: url }}
-            style={{ marginTop: 20 }}
+            // style={{ marginTop: 20 }}
+            onMessage={onMessage}
         />
 
     </View>
